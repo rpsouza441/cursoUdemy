31d9cb39ac2837febe1e1996d82f8f0c9fa55f9b
Rodrigo Pinheiro <rpsouza@hotmail.co.uk>
2020-10-17 17:10:09 -0300

    Projeto criado

---
+++ src/main/java/com/rodrigopinheiro/cursoUdemy/CursoUdemyApplication.java
@@ -0,0 +1,13 @@
+package com.rodrigopinheiro.cursoUdemy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoUdemyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoUdemyApplication.class, args);
+	}
+
+}
