e5963269e3c44993b8d7cfbc81e30f0f3d2159ca
Rodrigo Pinheiro <rpsouza@hotmail.co.uk>
2020-10-18 20:13:31 -0300

    Testando categoria de dominio

--- src/main/java/com/rodrigopinheiro/cursoUdemy/resources/CategoriaResource.java
+++ src/main/java/com/rodrigopinheiro/cursoUdemy/resources/CategoriaResource.java
@@ -1,16 +1,30 @@
 package com.rodrigopinheiro.cursoUdemy.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.rodrigopinheiro.cursoUdemy.domain.Categoria;
 
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriaResource {
 
 	@RequestMapping(method = RequestMethod.GET)
-	public String listar() {
-		return "rest está funcionando";
+	public List<Categoria> listar() {
+		
+		Categoria cat1 = new Categoria(1, "informatica");
+		Categoria cat2 = new Categoria(2, "escritorio");
+		
+		List<Categoria> lista = new  ArrayList<Categoria>();
+		lista.add(cat2);
+		lista.add(cat1);
+		
+		
+		return lista;
 	}
 
 }
