3eb6d5e6d71ed295ace6b2025ec4379357978054
Rodrigo Pinheiro <rpsouza@hotmail.co.uk>
2020-10-20 22:20:52 -0300

    criando mock em application

--- src/main/java/com/rodrigopinheiro/cursoUdemy/CursoUdemyApplication.java
+++ src/main/java/com/rodrigopinheiro/cursoUdemy/CursoUdemyApplication.java
@@ -1,13 +1,31 @@
 package com.rodrigopinheiro.cursoUdemy;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.rodrigopinheiro.cursoUdemy.domain.Categoria;
+import com.rodrigopinheiro.cursoUdemy.repositories.CategoriaRepository;
+
 @SpringBootApplication
-public class CursoUdemyApplication {
+public class CursoUdemyApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursoUdemyApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+
+		Categoria c1 = new Categoria(null, "informatica");
+		Categoria c2 = new Categoria(null, "escritorio");
+		categoriaRepository.saveAll(Arrays.asList(c1, c2));
 	}
 
 }
