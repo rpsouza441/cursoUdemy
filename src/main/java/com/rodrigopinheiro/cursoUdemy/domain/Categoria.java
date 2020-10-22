63877357bb26c24a2783658880f90ed9b52389de
Rodrigo Pinheiro <rpsouza@hotmail.co.uk>
2020-10-22 06:48:36 -0300

    ajuste no endpont /categoria/{id}

--- src/main/java/com/rodrigopinheiro/cursoUdemy/domain/Categoria.java
+++ src/main/java/com/rodrigopinheiro/cursoUdemy/domain/Categoria.java
@@ -10,6 +10,8 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToMany;
 
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+
 @Entity
 public class Categoria implements Serializable {
 
@@ -21,6 +23,7 @@ public class Categoria implements Serializable {
 
 	private String nome;
 	
+	@JsonManagedReference
 	@ManyToMany(mappedBy = "listaCategorias")
 	private List<Produto> listaProdutos = new ArrayList<Produto>();
 
--- src/main/java/com/rodrigopinheiro/cursoUdemy/services/CategoriaServices.java
+++ src/main/java/com/rodrigopinheiro/cursoUdemy/services/CategoriaServices.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import com.rodrigopinheiro.cursoUdemy.domain.Categoria;
 import com.rodrigopinheiro.cursoUdemy.repositories.CategoriaRepository;
+import com.rodrigopinheiro.cursoUdemy.services.Exception.ObjectNotFoundException;
 
 @Service
 public class CategoriaServices {
@@ -16,7 +17,8 @@ public class CategoriaServices {
 
 	public Categoria buscar(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
-		return obj.orElse(null);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
 
 	}
 
