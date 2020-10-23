2577ffe90ef45b1dfa6f925db3fbeee2a14f0de8
Rodrigo Pinheiro <rpsouza@hotmail.co.uk>
2020-10-22 21:04:21 -0300

    endpoint do cliente e endereco

--- src/main/java/com/rodrigopinheiro/cursoUdemy/domain/Cliente.java
+++ src/main/java/com/rodrigopinheiro/cursoUdemy/domain/Cliente.java
@@ -14,6 +14,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 import com.rodrigopinheiro.cursoUdemy.domain.enums.TipoCliente;
 
 @Entity
@@ -30,6 +31,7 @@ public class Cliente implements Serializable {
 	private Integer tipo;
 
 	@OneToMany(mappedBy = "cliente")
+	@JsonManagedReference
 	private List<Endereco> listaEndereco = new ArrayList<>();
 
 	
---
+++ src/main/java/com/rodrigopinheiro/cursoUdemy/services/ClienteServices.java
@@ -0,0 +1,25 @@
+package com.rodrigopinheiro.cursoUdemy.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.rodrigopinheiro.cursoUdemy.domain.Cliente;
+import com.rodrigopinheiro.cursoUdemy.repositories.ClienteRepository;
+import com.rodrigopinheiro.cursoUdemy.services.Exception.ObjectNotFoundException;
+
+@Service
+public class ClienteServices {
+
+	@Autowired
+	private ClienteRepository repo;
+
+	public Cliente buscar(Integer id) {
+		Optional<Cliente> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
+
+	}
+
+}
