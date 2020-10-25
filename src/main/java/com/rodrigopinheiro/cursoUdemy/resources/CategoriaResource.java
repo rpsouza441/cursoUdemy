2ed2e16d8b26bd538df43ae72a223f2274be3f8f
Rodrigo Pinheiro <rpsouza@hotmail.co.uk>
2020-10-25 11:42:01 -0300

    atualizando categoria com put e ajustes

--- src/main/java/com/rodrigopinheiro/cursoUdemy/resources/CategoriaResource.java
+++ src/main/java/com/rodrigopinheiro/cursoUdemy/resources/CategoriaResource.java
@@ -22,9 +22,17 @@ public class CategoriaResource {
 
 	@RequestMapping(value = "{id}", method = RequestMethod.GET)
 	public ResponseEntity<?> find(@PathVariable Integer id) {
-		Categoria obj = service.buscar(id);
+		Categoria obj = service.find(id);
 
 		return ResponseEntity.ok().body(obj);
+	}
+
+	@RequestMapping(value = "{id}", method = RequestMethod.PUT)
+	public ResponseEntity<Void> find(@RequestBody Categoria obj, @PathVariable Integer id) {
+		obj.setId(id);
+		obj = service.update(obj);
+		return ResponseEntity.noContent().build();
+
 	}
 
 	@RequestMapping(method = RequestMethod.POST)
