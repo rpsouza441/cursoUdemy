72f2f6b8dbf333967f5da5bd1ed8b3a2d641639b
Rodrigo Pinheiro <rpsouza@hotmail.co.uk>
2020-10-27 19:58:19 -0300

    personalisando o retorno da validação

---
+++ src/main/java/com/rodrigopinheiro/cursoUdemy/resources/exception/ValidationError.java
@@ -0,0 +1,25 @@
+package com.rodrigopinheiro.cursoUdemy.resources.exception;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ValidationError extends StandardError {
+
+	private static final long serialVersionUID = 1L;
+	private List<FieldMessage> erros = new ArrayList<>();
+
+	public ValidationError(Integer status, String msg, Long timeStamp) {
+		super(status, msg, timeStamp);
+
+	}
+
+	public List<FieldMessage> getErros() {
+		return erros;
+	}
+
+	public void addError(String fieldName, String message) {
+
+		this.erros.add(new FieldMessage(fieldName, message));
+	}
+
+}
