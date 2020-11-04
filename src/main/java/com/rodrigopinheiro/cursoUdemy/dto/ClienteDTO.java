7f1dc2a3f0ef9f391b48d762e9f1b71c08c44c3c
Rodrigo Pinheiro <rpsouza@hotmail.co.uk>
2020-11-04 20:27:17 -0300

    validacao cusmotimzada email no update

--- src/main/java/com/rodrigopinheiro/cursoUdemy/dto/ClienteDTO.java
+++ src/main/java/com/rodrigopinheiro/cursoUdemy/dto/ClienteDTO.java
@@ -2,41 +2,37 @@ package com.rodrigopinheiro.cursoUdemy.dto;
 
 import java.io.Serializable;
 
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotEmpty;
 
 import org.hibernate.validator.constraints.Length;
 
 import com.rodrigopinheiro.cursoUdemy.domain.Cliente;
+import com.rodrigopinheiro.cursoUdemy.services.validation.ClienteUpdate;
 
+@ClienteUpdate
 public class ClienteDTO implements Serializable {
-
 	private static final long serialVersionUID = 1L;
-
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	
 	private Integer id;
-	@NotEmpty(message = "Preenchimento Obrigatório")
-	@Length(min = 5, max = 120, message = "O tamanho deve ser entre 5 e 120 caracteres")
+	
+	@NotEmpty(message="Preenchimento obrigatório")
+	@Length(min=5, max=120, message="O tamanho deve ser entre 5 e 120 caracteres")
 	private String nome;
-	@NotEmpty(message = "Preenchimento obrigatório")
-	@Email(message = "Email invalido")
+	
+	@NotEmpty(message="Preenchimento obrigatório")
+	@Email(message="Email inválido")
 	private String email;
-
+	
 	public ClienteDTO() {
-		super();
 	}
 
 	public ClienteDTO(Cliente obj) {
-		super();
-		this.id = obj.getId();
-		this.nome = obj.getNome();
-		this.email = obj.getEmail();
+		id = obj.getId();
+		nome = obj.getNome();
+		email = obj.getEmail();
 	}
-
+	
 	public Integer getId() {
 		return id;
 	}
@@ -60,5 +56,4 @@ public class ClienteDTO implements Serializable {
 	public void setEmail(String email) {
 		this.email = email;
 	}
-
 }
