c7fdc62d360d5bb35e2232d96c633e19de881765
Rodrigo Pinheiro <rpsouza@hotmail.co.uk>
2020-11-15 14:08:39 -0300

    criando profile de dev

---
+++ src/main/java/com/rodrigopinheiro/cursoUdemy/config/DevConfig.java
@@ -0,0 +1,34 @@
+package com.rodrigopinheiro.cursoUdemy.config;
+
+import java.text.ParseException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.rodrigopinheiro.cursoUdemy.services.DBService;
+
+@Configuration
+@Profile("dev")
+public class DevConfig {
+
+	@Autowired
+	private DBService dbservice;
+
+	@Value("${spring.jpa.hibernate.ddl-auto}")
+	private String strategy;
+
+	@Bean
+	public boolean instantiateDatabase() throws ParseException {
+
+		if (!"create".equals(strategy)) {
+			return false;
+
+		}
+		dbservice.instantiateTestDatabase();
+		return true;
+	}
+
+}
