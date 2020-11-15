156ed8db7755c3cebd6fc5ebf15730c3dea14134
Rodrigo Pinheiro <rpsouza@hotmail.co.uk>
2020-11-15 16:10:47 -0300

    Implementando SmtpEmailService com servidor do Google

--- src/main/java/com/rodrigopinheiro/cursoUdemy/config/DevConfig.java
+++ src/main/java/com/rodrigopinheiro/cursoUdemy/config/DevConfig.java
@@ -9,6 +9,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 import com.rodrigopinheiro.cursoUdemy.services.DBService;
+import com.rodrigopinheiro.cursoUdemy.services.EmailService;
+import com.rodrigopinheiro.cursoUdemy.services.SMTPEmailService;
 
 @Configuration
 @Profile("dev")
@@ -29,6 +31,11 @@ public class DevConfig {
 		}
 		dbservice.instantiateTestDatabase();
 		return true;
+	}
+	
+	@Bean
+	public EmailService emailService() {
+		return new SMTPEmailService();
 	}
 
 }
